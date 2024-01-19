3fe3587456fb277c0586cb0dcb09f765d8e21585
Syyren <129496054+Syyren@users.noreply.github.com>
2024-01-18 23:19:48 -0700

    created application

---
+++ docs/script.js
@@ -0,0 +1,41 @@
+let $ = function (id) { return document.getElementById(id) }
+
+function processEntry()
+{
+    let income = $("income").value;
+    if (income > 0)
+    {
+        income = parseFloat(income);
+        $("owed").textContent = "$" + calculateTax(income).toFixed(2);
+    }
+    else if (isNaN(income))
+    {
+        alert("Error: Entry is invalid. Please enter a number.");
+    }
+    else
+    {
+        alert("Error: Entry is invalid. Please enter a number greater than zero.");
+    }
+}
+
+function calculateTax(income)
+{
+    const BRKTS = [9225, 37450, 90750, 189300, 411500, 413200, 0];
+    const BRKT_TAXES = [0.10, 0.15, 0.25, 0.28, 0.33, 0.35, 0.396];
+    let amountOwed = 0, remainder = 0;
+    for (let i in BRKTS)
+    {
+        remainder = income - BRKTS[i]
+        if (remainder < 0 || remainder == income)
+        {
+            amountOwed += (income * BRKT_TAXES[i]);
+            break;
+        }
+        else
+        {
+            amountOwed += ((income - remainder) * BRKT_TAXES[i]);
+            income = remainder;
+        }
+    }
+    return amountOwed;
+}
